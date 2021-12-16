87e5deefc402bf34f64278a6f314332a68e91992
omer5423 <omer.ali.5423@gmail.com>
2021-12-16 14:45:00 +0000

    deploay heroku

---
+++ index.js
@@ -0,0 +1,7 @@
+if(process.env.NODE_ENV !== 'production' require('dotenv').config()
+
+const app = require('./src/app')
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, ()=> console.log(`server is working in port ${PORT}  `))
